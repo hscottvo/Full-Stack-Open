ec8d0cb4e7b6d06ca952cf5634420da31bf8bf9e
hscottvo <56327086+hscottvo@users.noreply.github.com>
2023-07-10 11:32:49 -0700

    Add exercises 1.3-1.5

--- exercise_1/src/App.js
+++ exercise_1/src/App.js
@@ -1,54 +1,60 @@
 const Header = (props) => {
-    return (
-        <h1>{props.title}</h1>
-    )
-}
+  return <h1>{props.title}</h1>;
+};
 
 const Part = (props) => {
-    return (
-        <p>
-            {props.partName} {props.numExercises}
-        </p>
-    )
-}
+  return (
+    <p>
+      {props.partName} {props.numExercises}
+    </p>
+  );
+};
 
 const Content = (props) => {
-    return (
-        <div>
-            <Part partName={props.name1} numExercises={props.ex1} />
-            <Part partName={props.name2} numExercises={props.ex2} />
-            <Part partName={props.name3} numExercises={props.ex3} />
-        </div>
-    )
-}
+  let partsList = []
+  props.parts.forEach((part, index) => {
+    partsList.push(<Part key={"part" + index} partName={part.name} numExercises={part.exercises} />)
+  });
+  return (
+    <div>
+      {partsList}
+    </div>
+  );
+};
 
 const Total = (props) => {
-    const sum = props.exerciseList.reduce((a, b) => { return a + b }, 0)
-    return (
-        <p>
-            Number of exercises {sum}
-        </p>
-    )
-}
-
+  const sum = props.exerciseList.reduce((previous, curr) => {
+    return previous + curr.exercises;
+  }, 0);
+  return <p>Number of exercises {sum}</p>;
+};
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ]
+  };
 
-    return (
-        <>
-            <Header title={course} />
-            <Content name1={part1} name2={part2} name3={part3}
-                ex1={exercises1} ex2={exercises2} ex3={exercises3} />
-            <Total exerciseList={[exercises1, exercises2, exercises3]} />
-        </>
-    )
-}
+  return (
+    <>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total exerciseList={course.parts} />
+    </>
+  );
+};
 
-export default App
+export default App;
