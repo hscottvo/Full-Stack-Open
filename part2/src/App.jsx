85f954190498ce978a5b2f8dc1c18f23db4760f9
hscottvo <56327086+hscottvo@users.noreply.github.com>
2025-04-01 11:04:53 -0700

    Format

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,20 +1,25 @@
-import { Notes, Phonebook, ServerData, PhonebookAxios } from "./components/Archive"
-import Countries from "./components/Countries"
+import {
+  Notes,
+  Phonebook,
+  ServerData,
+  PhonebookAxios,
+} from "./components/Archive";
+import Countries from "./components/Countries";
 
 const App = () => {
-    let render = "countries"
-    switch (render) {
-        case "notes":
-            return <Notes />
-        case "phonebook":
-            return <Phonebook />
-        case "data_from_server":
-            return <ServerData />
-        case "phonebook_axios":
-            return <PhonebookAxios />
-        case "countries":
-            return <Countries />
-    }
-}
+  let render = "PhonebookAxios";
+  switch (render) {
+    case "notes":
+      return <Notes />;
+    case "phonebook":
+      return <Phonebook />;
+    case "data_from_server":
+      return <ServerData />;
+    case "phonebook_axios":
+      return <PhonebookAxios />;
+    case "countries":
+      return <Countries />;
+  }
+};
 
-export default App
+export default App;
