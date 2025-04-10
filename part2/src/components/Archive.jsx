8423f862d348b0c8db04bff16c07af422f4c2224
hscottvo <56327086+hscottvo@users.noreply.github.com>
2025-04-10 10:35:43 -0700

    Update to exercise 3.19

--- part2/src/components/Archive.jsx
+++ part2/src/components/Archive.jsx
@@ -1,324 +1,335 @@
-import { useState, useEffect } from "react";
-import "../index.css";
-import axios from "axios";
-import Note from "./Note";
-import PhonebookService from "../services/PhonebookService";
+import { useState, useEffect } from "react"
+import "../index.css"
+import axios from "axios"
+import Note from "./Note"
+import PhonebookService from "../services/PhonebookService"
 const Notes = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState("")
+    const [showAll, setShowAll] = useState(true)
 
-  let handleNoteChange = (event) => {
-    console.log("event:", event.target.value);
-    setNewNote(event.target.value);
-  };
+    let handleNoteChange = (event) => {
+        console.log("event:", event.target.value)
+        setNewNote(event.target.value)
+    }
 
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
-    };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
-  };
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            important: Math.random() < 0.5,
+            id: String(notes.length + 1),
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote("")
+    }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important === true)
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  );
-};
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    )
+}
 
 const Filter = ({ filter, handleFilterChange }) => {
-  return (
-    <div>
-      filter shown with
-      <input value={filter} onChange={handleFilterChange} />
-    </div>
-  );
-};
+    return (
+        <div>
+            filter shown with
+            <input value={filter} onChange={handleFilterChange} />
+        </div>
+    )
+}
 
 const PersonForm = ({
-  handleSubmit,
-  newName,
-  handleNameChange,
-  newPhoneNumber,
-  handlePhoneNumberChange,
+    handleSubmit,
+    newName,
+    handleNameChange,
+    newPhoneNumber,
+    handlePhoneNumberChange,
 }) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number:{" "}
-        <input value={newPhoneNumber} onChange={handlePhoneNumberChange} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  );
-};
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                name: <input value={newName} onChange={handleNameChange} />
+            </div>
+            <div>
+                number:{" "}
+                <input value={newPhoneNumber} onChange={handlePhoneNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
 
 const Persons = ({ people, setPeople, filter, messageHandler }) => {
-  const handleDeletePerson = (id, name) => {
-    const deleteResponse = PhonebookService.deleteNumber(
-      id,
-      name,
-      messageHandler,
-    );
-    deleteResponse.then((response) => {
-      setPeople(people.filter((obj) => obj.id !== id));
-    });
-  };
-
-  return (
-    <>
-      {people
-        .filter((person) =>
-          person.name.toLowerCase().includes(filter.toLowerCase()),
+    const handleDeletePerson = (id, name) => {
+        const deleteResponse = PhonebookService.deleteNumber(
+            id,
+            name,
+            messageHandler,
         )
-        .map((person) => (
-          <div key={person.name}>
-            {person.name} {person.number}, it is{" "}
-            {person.number?.length % 2 == 1 ? "odd" : "even"}
-            <button onClick={() => handleDeletePerson(person.id, person.name)}>
-              delete
-            </button>
-          </div>
-        ))}
-    </>
-  );
-};
+        deleteResponse.then((response) => {
+            setPeople(people.filter((obj) => obj.id !== id))
+        })
+    }
+
+    return (
+        <>
+            {people
+                .filter((person) =>
+                    person.name.toLowerCase().includes(filter.toLowerCase()),
+                )
+                .map((person) => (
+                    <div key={person.name}>
+                        {person.name} {person.number}, it is{" "}
+                        {person.number?.length % 2 == 1 ? "odd" : "even"}
+                        <button onClick={() => handleDeletePerson(person.id, person.name)}>
+                            delete
+                        </button>
+                    </div>
+                ))}
+        </>
+    )
+}
 
 const Phonebook = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-  const [filter, setFilter] = useState("");
-  const [newName, setNewName] = useState("");
-  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    ])
+    const [filter, setFilter] = useState("")
+    const [newName, setNewName] = useState("")
+    const [newPhoneNumber, setNewPhoneNumber] = useState("")
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handlePhoneNumberChange = (event) => {
-    setNewPhoneNumber(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(persons);
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    } else {
-      setPersons(persons.concat({ name: newName, number: newPhoneNumber }));
-      setNewName("");
-      setNewPhoneNumber("");
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
     }
-  };
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <h3>add a new</h3>
-      <PersonForm
-        handleSubmit={handleSubmit}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newPhoneNumber={newPhoneNumber}
-        handlePhoneNumberChange={handlePhoneNumberChange}
-      />
-      <h3>Numbers</h3>
-      <Persons people={persons} filter={filter} />
-    </div>
-  );
-};
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const handlePhoneNumberChange = (event) => {
+        setNewPhoneNumber(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(persons)
+        if (persons.some((person) => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`)
+            return
+        } else {
+            setPersons(persons.concat({ name: newName, number: newPhoneNumber }))
+            setNewName("")
+            setNewPhoneNumber("")
+        }
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter filter={filter} handleFilterChange={handleFilterChange} />
+            <h3>add a new</h3>
+            <PersonForm
+                handleSubmit={handleSubmit}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newPhoneNumber={newPhoneNumber}
+                handlePhoneNumberChange={handlePhoneNumberChange}
+            />
+            <h3>Numbers</h3>
+            <Persons people={persons} filter={filter} />
+        </div>
+    )
+}
 
 const ServerData = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true);
+    const [notes, setNotes] = useState([])
+    const [newNote, setNewNote] = useState("")
+    const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
-    });
-  };
+    const hook = () => {
+        console.log("effect")
+        axios.get("http://localhost:3001/notes").then((response) => {
+            console.log("promise fulfilled")
+            setNotes(response.data)
+        })
+    }
 
-  useEffect(hook, []);
-  console.log("render", notes.length, "notes");
+    useEffect(hook, [])
+    console.log("render", notes.length, "notes")
 
-  let handleNoteChange = (event) => {
-    console.log("event:", event.target.value);
-    setNewNote(event.target.value);
-  };
+    let handleNoteChange = (event) => {
+        console.log("event:", event.target.value)
+        setNewNote(event.target.value)
+    }
 
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
-    };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
-  };
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            important: Math.random() < 0.5,
+            id: String(notes.length + 1),
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote("")
+    }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+    const notesToShow = showAll
+        ? notes
+        : notes.filter((note) => note.important === true)
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? "important" : "all"}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
-    </div>
-  );
-};
+    return (
+        <div>
+            <h1>Notes</h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)}>
+                    show {showAll ? "important" : "all"}
+                </button>
+            </div>
+            <ul>
+                {notesToShow.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange} />
+                <button type="submit">save</button>
+            </form>
+        </div>
+    )
+}
 
 const NumNotification = ({ message, type }) => {
-  if (message === null) {
-    return null;
-  }
-  console.log("poopoo", message, type);
-  switch (type) {
-    case "add":
-      return <div className="added">Added {message}</div>;
-    case "alreadyDeleted":
-      return (
-        <div className="alreadyDeleted">
-          Information of {message} has already been removed from server
-        </div>
-      );
-  }
-};
+    if (message === null) {
+        return null
+    }
+    switch (type) {
+        case "add":
+            return <div className="added">Added {message}</div>
+        case "alreadyDeleted":
+            return (
+                <div className="error">
+                    Information of {message} has already been removed from server
+                </div>
+            )
+        case "invalidData":
+            console.log(message)
+            return (
+                <div className="error">
+                    Person validation failed: {message}
+                </div>
+            )
+    }
+}
 
 const PhonebookAxios = () => {
-  const [persons, setPersons] = useState([]);
-  const [filter, setFilter] = useState("");
-  const [newName, setNewName] = useState("");
-  const [newPhoneNumber, setNewPhoneNumber] = useState("");
-  const [message, setMessage] = useState(null);
-  const [type, setType] = useState("");
+    const [persons, setPersons] = useState([])
+    const [filter, setFilter] = useState("")
+    const [newName, setNewName] = useState("")
+    const [newPhoneNumber, setNewPhoneNumber] = useState("")
+    const [message, setMessage] = useState(null)
+    const [type, setType] = useState("")
 
-  useEffect(() => {
-    PhonebookService.getAll().then((response) => {
-      setPersons(response);
-    });
-  }, []);
+    useEffect(() => {
+        PhonebookService.getAll().then((response) => {
+            setPersons(response)
+        })
+    }, [])
 
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
 
-  const handlePhoneNumberChange = (event) => {
-    setNewPhoneNumber(event.target.value);
-  };
+    const handlePhoneNumberChange = (event) => {
+        setNewPhoneNumber(event.target.value)
+    }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(persons);
-    const newObject = { name: newName, number: newPhoneNumber };
-    const objectResponse = PhonebookService.createNumber(newObject);
-    objectResponse.then((returnedObject) => {
-      if (returnedObject != null) {
-        console.log("new");
-        if (persons.some((person) => person.id === returnedObject.id)) {
-          setPersons(
-            persons.map((person) =>
-              person.id === returnedObject.id ? returnedObject : person,
-            ),
-          );
-        } else {
-          setPersons(persons.concat(returnedObject));
-        }
-        setNewName("");
-        setNewPhoneNumber("");
-        setMessage(newName);
-        setType("add");
-      }
-    });
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(persons)
+        const newObject = { name: newName, number: newPhoneNumber }
+        const objectResponse = PhonebookService.createNumber(newObject)
+        objectResponse
+            .then((returnedObject) => {
+                console.log(returnedObject)
+                if (returnedObject != null) {
+                    console.log("new")
+                    if (persons.some((person) => person.id === returnedObject.id)) {
+                        setPersons(
+                            persons.map((person) =>
+                                person.id === returnedObject.id ? returnedObject : person,
+                            ),
+                        )
+                    } else {
+                        setPersons(persons.concat(returnedObject))
+                    }
+                    setNewName("")
+                    setNewPhoneNumber("")
+                    setMessage(newName)
+                    setType("add")
+                }
+            })
+            .catch(error => {
+                console.log(error.response.data.error)
+                messageHandler(error.response.data.error, "invalidData")
+            })
+    }
 
-  const messageHandler = (message, type) => {
-    console.log(message, type);
-    setMessage(message);
-    setType(type);
-  };
+    const messageHandler = (message, type) => {
+        setMessage(message)
+        setType(type)
+    }
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <NumNotification message={message} type={type} />
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      <h3>add a new</h3>
-      <PersonForm
-        handleSubmit={handleSubmit}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newPhoneNumber={newPhoneNumber}
-        handlePhoneNumberChange={handlePhoneNumberChange}
-      />
-      <h3>Numbers</h3>
-      <Persons
-        people={persons}
-        setPeople={setPersons}
-        filter={filter}
-        messageHandler={messageHandler}
-      />
-    </div>
-  );
-};
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <NumNotification message={message} type={type} />
+            <Filter filter={filter} handleFilterChange={handleFilterChange} />
+            <h3>add a new</h3>
+            <PersonForm
+                handleSubmit={handleSubmit}
+                newName={newName}
+                handleNameChange={handleNameChange}
+                newPhoneNumber={newPhoneNumber}
+                handlePhoneNumberChange={handlePhoneNumberChange}
+            />
+            <h3>Numbers</h3>
+            <Persons
+                people={persons}
+                setPeople={setPersons}
+                filter={filter}
+                messageHandler={messageHandler}
+            />
+        </div>
+    )
+}
 
-export { Notes, Phonebook, ServerData, PhonebookAxios };
+export { Notes, Phonebook, ServerData, PhonebookAxios }
--- part2/src/services/PhonebookService.jsx
+++ part2/src/services/PhonebookService.jsx
@@ -1,16 +1,16 @@
-import axios from "axios";
-// const baseUrl = "http://localhost:3001/api/persons";
-const baseUrl = "/api/persons";
+import axios from "axios"
+// const baseUrl = "http://localhost:3001/api/persons"
+const baseUrl = "/api/persons"
 
 const getAll = () => {
     return axios
         .get(baseUrl)
         .then((response) => response.data)
         .catch((error) => {
-            console.log(error);
-            alert(`Unable to read numbers from ${baseUrl}: ${error}`);
-        });
-};
+            console.log(error)
+            alert(`Unable to read numbers from ${baseUrl}: ${error}`)
+        })
+}
 
 const createNumber = (newObject) => {
     return getAll().then((getAllResponse) => {
@@ -20,24 +20,24 @@ const createNumber = (newObject) => {
             if (window.confirm(confirmString)) {
                 const oldObject = getAllResponse.filter(
                     (obj) => obj.name === newObject.name,
-                )[0];
-                newObject = { ...newObject, id: oldObject.id };
+                )[0]
+                newObject = { ...newObject, id: oldObject.id }
                 return axios
                     .put(`${baseUrl}/${oldObject.id}`, newObject)
-                    .then((response) => response.data);
+                    .then((response) => response.data)
             } else {
-                return null;
+                return null
             }
         } else {
-            return axios.post(baseUrl, newObject).then((response) => response.data);
+            return axios.post(baseUrl, newObject).then((response) => response.data)
         }
-    });
-};
+    })
+}
 
 const deleteNumber = (id, name, handler) => {
     return axios.delete(`${baseUrl}/${id}`).catch((_) => {
-        handler(name, "alreadyDeleted");
-    });
-};
+        handler(name, "alreadyDeleted")
+    })
+}
 
-export default { getAll, createNumber, deleteNumber };
+export default { getAll, createNumber, deleteNumber }
