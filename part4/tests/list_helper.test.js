52aa8805b55463f1f7fb2242a6d5c69c74d2a225
hscottvo <56327086+hscottvo@users.noreply.github.com>
2025-04-11 12:01:06 -0700

    Update to exercise 4.4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-import { test } from "node:test"
+import { test, describe } from "node:test"
 import assert from "node:assert"
 import listHelper from "../utils/list_helper.js"
 
@@ -7,4 +7,51 @@ test("dummy returns one", () => {
     const result = listHelper.dummy(blogs)
 
     assert.strictEqual(result, 1)
+})
+
+describe("total likes", () => {
+    const emptyList = []
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+    const listWithTwoBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: 'ietrsnietnrsietr',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 11,
+            __v: 0
+        }
+    ]
+
+    test("when list is empty, equals 0", () => {
+        const result = listHelper.totalLikes(emptyList)
+        assert.strictEqual(result, 0)
+    })
+
+    test("when list has only one blog, equals the likes of that blog", () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+
+    test("when list has multiple blogs, equals the likes of those blogs", () => {
+        const result = listHelper.totalLikes(listWithTwoBlogs)
+        assert.strictEqual(result, 16)
+    })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,12 @@ const dummy = () => {
     return 1
 }
 
-export default { dummy }
+const totalLikes = (blogList) => {
+    const reducer = (acc, val) => {
+        return acc + val.likes
+    }
+
+    return blogList.reduce(reducer, 0)
+}
+
+export default { dummy, totalLikes }
