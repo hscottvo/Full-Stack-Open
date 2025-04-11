5773dacf2b3451d7a8c8620ee8172696fa1d9776
hscottvo <56327086+hscottvo@users.noreply.github.com>
2025-04-11 12:09:41 -0700

    Update to exercise 4.5

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -9,36 +9,53 @@ test("dummy returns one", () => {
     assert.strictEqual(result, 1)
 })
 
+const emptyList = []
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    }
+]
+const listWithFourBlogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 1,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 11,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: 'ietrsnietnrsietr',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 11,
+        __v: 0
+    }
+]
+
 describe("total likes", () => {
-    const emptyList = []
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        }
-    ]
-    const listWithTwoBlogs = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: 'ietrsnietnrsietr',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 11,
-            __v: 0
-        }
-    ]
 
     test("when list is empty, equals 0", () => {
         const result = listHelper.totalLikes(emptyList)
@@ -51,7 +68,26 @@ describe("total likes", () => {
     })
 
     test("when list has multiple blogs, equals the likes of those blogs", () => {
-        const result = listHelper.totalLikes(listWithTwoBlogs)
-        assert.strictEqual(result, 16)
+        const result = listHelper.totalLikes(listWithFourBlogs)
+        assert.strictEqual(result, 28)
+    })
+})
+
+describe("favorite blog", () => {
+
+    test("when list is empty, equals undefined", () => {
+        const result = listHelper.favoriteBlog(emptyList)
+
+        assert.deepStrictEqual(result, undefined)
+    })
+
+    test("when list has only one blog, equals that blog", () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        assert.deepStrictEqual(result, listWithOneBlog[0])
+    })
+
+    test("when list has multiple blogs, equals the first most-liked blog", () => {
+        const result = listHelper.favoriteBlog(listWithFourBlogs)
+        assert.deepStrictEqual(result, listWithFourBlogs[1])
     })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -10,4 +10,21 @@ const totalLikes = (blogList) => {
     return blogList.reduce(reducer, 0)
 }
 
-export default { dummy, totalLikes }
+const favoriteBlog = (blogList) => {
+    const reducer = (acc, val) => {
+        if (!acc) {
+            return val
+        }
+
+        if (acc.likes < val.likes) {
+            return val
+        }
+        else {
+            return acc
+        }
+    }
+
+    return blogList.reduce(reducer, undefined)
+}
+
+export default { dummy, totalLikes, favoriteBlog }
